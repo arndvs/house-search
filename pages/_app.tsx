85f2c13a5d337b2a532d5a1ae88514fc83038714
Aaron Davis <aaron@ripemetrics.com>
2022-11-29 10:42:59 -0800

    babel config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from "next/app";
-// import Head from "next/head";
+import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />; // global providers i.e. auth provider, apollo provider
+  return <>
+    <Head>
+          <title>Home Sweet Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      <Component {...pageProps} />
+      {/* global providers i.e. auth provider, apollo provider */}
+  </>;
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,15 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+
+interface IProps {
+    main: ReactNode;
+  }
+
+  const Layout: FunctionComponent<IProps> = ({ main }) => {
+    // const { logout, authenticated } = useAuth();
+    return ( <div className="max-w-screen-2xl mx-auto text-white bg-red-500">{main}</div>
+    );
+};
+  export default Layout;
